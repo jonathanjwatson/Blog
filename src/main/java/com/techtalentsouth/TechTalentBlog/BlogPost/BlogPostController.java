3b3e6c0a24d50834e6723a73a2def0f9da899d64
Jonathan J. Watson <jonathanwatson1@gmail.com>
2022-07-26 20:53:49 -0400

    delete is working

--- src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,31 +22,30 @@ public class BlogPostController {
 
 	@GetMapping(value = "/")
 	public String index(BlogPost blogPost, Model model) {
-		model.addAttribute("posts", posts);
+		model.addAttribute("posts", blogPostRepository.findAll());
 		return "blogpost/index";
 	}
-	
+
 	@GetMapping(value = "/blogposts/new")
-	public String newBlog (BlogPost blogPost) {
-	    return "blogpost/new";
+	public String newBlog(BlogPost blogPost) {
+		return "blogpost/new";
 	}
 
 	private BlogPost blogPost;
 
 	@PostMapping(value = "/blogposts")
 	public String addNewBlogPost(BlogPost blogPost, Model model) {
-		blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
-		posts.add(blogPost);
+		BlogPost savedBlogPost = blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+		posts.add(savedBlogPost);
 		model.addAttribute("title", blogPost.getTitle());
 		model.addAttribute("author", blogPost.getAuthor());
 		model.addAttribute("blogEntry", blogPost.getBlogEntry());
 		return "blogpost/result";
 	}
-	
+
 	@RequestMapping(value = "/blogposts/{id}", method = RequestMethod.DELETE)
 	public String deletePostWithId(@PathVariable Long id, BlogPost blogPost) {
-
-	    blogPostRepository.deleteById(id);
-	    return "blogpost/index";
+		blogPostRepository.deleteById(id);
+		return "redirect:/";
 	}
 }
