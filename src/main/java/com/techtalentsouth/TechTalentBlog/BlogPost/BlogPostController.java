4703480147d729fb9aea748be6b2f82f43c633aa
Jonathan J. Watson <jonathanwatson1@gmail.com>
2022-07-28 18:56:15 -0400

    added h2 persistence and single blogpost controller logic

--- src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
@@ -2,6 +2,7 @@ package com.techtalentsouth.TechTalentBlog.BlogPost;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,6 +27,22 @@ public class BlogPostController {
 		return "blogpost/index";
 	}
 
+	// TODO: Read a single blog post at a time.
+
+	@GetMapping(value = "/blogposts/{id}")
+	public String viewPostById(@PathVariable Long id, BlogPost blogpost, Model model) {
+		// 1. I need the blogpost data for that id.
+		Optional<BlogPost> optionalBlogPost = blogPostRepository.findById(id);
+		// FIXME: What if there is no blogpost found with that id?
+		BlogPost blogPost = optionalBlogPost.get();
+		// 2. I need to display the result page.
+			model.addAttribute("title", blogPost.getTitle());
+			model.addAttribute("author", blogPost.getAuthor());
+			model.addAttribute("blogEntry", blogPost.getBlogEntry());
+
+		return "blogpost/result";
+	}
+
 	@GetMapping(value = "/blogposts/new")
 	public String newBlog(BlogPost blogPost) {
 		return "blogpost/new";
@@ -33,13 +50,18 @@ public class BlogPostController {
 
 	@PostMapping(value = "/blogposts")
 	public String addNewBlogPost(BlogPost blogPost, Model model) {
-		BlogPost savedBlogPost = blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+		BlogPost savedBlogPost = blogPostRepository
+				.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
 		model.addAttribute("title", savedBlogPost.getTitle());
 		model.addAttribute("author", savedBlogPost.getAuthor());
 		model.addAttribute("blogEntry", savedBlogPost.getBlogEntry());
 		// TODO: Build the single blogpost page, and redirect there instead.
 		return "blogpost/result";
 	}
+
+	// TODO: GET Route to display the Edit page
+
+	// TODO: PUT Route to actually edit the page from the Edit page form submission
 
 	@RequestMapping(value = "/blogposts/{id}", method = RequestMethod.DELETE)
 	public String deletePostWithId(@PathVariable Long id, BlogPost blogPost) {
